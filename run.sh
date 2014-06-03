0071930394c60a2fa792f84e5e18707f436f193e
James Badger <jamesbadger@gmail.com>
2014-06-03 14:00:13 -0600

    Run redis-server from /usr/local/bin

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ if [ ! -f /.redis_password_set ]; then
 	/set_redis_password.sh
 fi
 
-exec /usr/bin/redis-server /etc/redis/redis_default.conf
+exec /usr/local/bin/redis-server /etc/redis/redis_default.conf
